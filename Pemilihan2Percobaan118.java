6ebeb99838a9050e8d8293842d9c635b9b9f15eb
Calon-raja-ngoding <nbilhnief@gmail.com>
2024-10-03 09:50:52 +0800

    reivisi2

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -9,9 +9,11 @@ public class Pemilihan2Percobaan118 {
 
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
-                System.out.println("Tahun kabisat");
-            }  
-            else { System.out.println("Bukan tahun kabisat");}  
+                System.out.println("Tahun kabisat");}  
+            else { if ((tahun % 400) == 0) {
+                System.out.println("Tahun kabisat");}
+            else {
+                System.out.println("Bukan tahun kabisat");}}  
         } else {
             System.out.println("Bukan tahun kabisat");
         }
