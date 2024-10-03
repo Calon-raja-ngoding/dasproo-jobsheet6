0d6e0dfc2d56f509f253b4fb30ee2a7b1a19d133
Calon-raja-ngoding <nbilhnief@gmail.com>
2024-10-03 08:53:44 +0800

    jobsheet6

---
+++ Pemilihan2Percobaan118.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan118 {
+    public static void main(String[] args) {
+        Scanner input18 = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan tahun : ");
+        tahun = input18.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun kabisat");
+            }    
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
